cba9d853f436ad02607b3b4a9126c762886a275b
joh-brunner <joh.brunner@fau.de>
2026-01-25 12:19:56 +0100

    init glacier data from oggm shop gridded data

--- glacier_data.py
+++ glacier_data.py
@@ -1,21 +1,39 @@
 import xarray as xr
 import numpy as np
 
-# Maybe Geometry should be split here
+# Maybe Geometry should be split here from things like divflux and climatic_mb
 
 
 class GlacierData:
-    def __init__(self, ny, nx, dx):
-        self.ny = ny
+    def __init__(self):
+        pass
+
+    def init(self, nx, ny, dx, bed_topo, ice_thickness):
         self.nx = nx
+        self.ny = ny
         self.dx = dx
         self.data = xr.Dataset(
             {
-                "bed_topography": (("y", "x"), np.zeros((ny, nx), dtype=np.float32)),
-                "ice_thickness": (("y", "x"), np.zeros((ny, nx), dtype=np.float32)),
-                "surface_h": (("y", "x"), np.zeros((ny, nx), dtype=np.float32)), # redundant
-                "divflux": (("y", "x"), np.zeros((ny, nx), dtype=np.float32)),
-                "climatic_mb": (("y", "x"), np.zeros((ny, nx), dtype=np.float32)),
+                "bed_topography": (("y", "x"), bed_topo.data),
+                "ice_thickness": (("y", "x"), ice_thickness.data),
             },
             coords={"x": np.arange(nx), "y": np.arange(ny)},
         )
+
+    def init_from_gridded_data(self, gridded_nc, thickness="consensus_ice_thickness"):
+        ds = xr.open_dataset(gridded_nc)
+        nx = ds["x"].shape[0]
+        ny = ds["y"].shape[0]
+        dx = ds.attrs["dx"]
+        bed_topo = ds["topo_smoothed"] - ds[thickness]
+        ice_thickness = ds[thickness]
+        self.init(nx,ny, dx, bed_topo, ice_thickness)
+        ds.close()
+
+    def store_data(self, out_path):
+        self.data.to_netcdf(out_path)
+        
+
+glacier = GlacierData()
+glacier.init_from_gridded_data("data/input/gridded_data.nc")
+glacier.store_data("data/output/glacier_data.nc")
